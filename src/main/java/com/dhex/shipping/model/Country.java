eeb1083f2ee6b46d8d3155b16d70f078b920b8b4
Paolo Carrasco <paolo.carrasco@gmail.com>
2016-07-23 18:15:52 -0500

    country doesn't implement the comparable interface

--- src/main/java/com/dhex/shipping/model/Country.java
+++ src/main/java/com/dhex/shipping/model/Country.java
@@ -1,6 +1,6 @@
 package com.dhex.shipping.model;
 
-public class Country implements Comparable<Country> {
+public class Country {
 
     private String name;
 
@@ -16,31 +16,4 @@ public class Country implements Comparable<Country> {
         this.name = name;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Country country = (Country) o;
-
-        return name != null ? name.equals(country.name) : country.name == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        return name != null ? name.hashCode() : 0;
-    }
-
-    @Override
-    public int compareTo(Country anotherCountry) {
-        if (anotherCountry == null) {
-            return 1;
-        }
-        return getName().compareToIgnoreCase(anotherCountry.getName());
-    }
 }
