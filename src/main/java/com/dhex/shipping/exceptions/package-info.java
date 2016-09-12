37164a6949029409f2bfe1fded47355fac949a9e
Paolo Carrasco <paolo.carrasco@gmail.com>
2016-09-12 02:36:49 -0500

    new packages are explained in their package-info files

---
+++ src/main/java/com/dhex/shipping/exceptions/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * It contains possible exceptions that can be thrown throughout the app.
+ */
+package com.dhex.shipping.exceptions;
---
+++ src/main/java/com/dhex/shipping/services/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * It contains service classes that manage the logic between the domain classes.
+ */
+package com.dhex.shipping.services;
