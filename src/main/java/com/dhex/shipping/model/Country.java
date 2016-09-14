f57c0255a8ba1de63af85657dc1ddb38815df52b
Paolo Carrasco <paolo.carrasco@gmail.com>
2016-09-13 19:44:45 -0500

    country is created

--- src/main/java/com/dhex/shipping/model/Country.java
+++ src/main/java/com/dhex/shipping/model/Country.java
@@ -7,4 +7,22 @@ public class Country {
     private long id;
     private String name;
     private boolean enabled;
+
+    public Country(long id, String name, boolean enabled) {
+        this.id = id;
+        this.name = name;
+        this.enabled = enabled;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
