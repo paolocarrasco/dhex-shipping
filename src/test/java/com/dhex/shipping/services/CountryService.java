328868f416347b92852a673e95203b285e43ea95
Paolo Carrasco <paolo.carrasco@gmail.com>
2016-09-14 00:24:54 -0500

    country service validates the name when creating

--- src/test/java/com/dhex/shipping/services/CountryService.java
+++ src/test/java/com/dhex/shipping/services/CountryService.java
@@ -1,9 +1,12 @@
 package com.dhex.shipping.services;
 
 import com.dhex.shipping.dao.CountryDao;
+import com.dhex.shipping.exceptions.InvalidArgumentDhexException;
 import com.dhex.shipping.model.Country;
 
 public class CountryService {
+    private static final String EMPTY_NAME_ERROR_MESSAGE = "Name of the country should not be empty";
+    private static final String LARGE_NAME_ERROR_MESSAGE = "Name of the country should not be greater than 100 chars";
     private CountryDao countryDao;
 
     public CountryService(CountryDao countryDao) {
@@ -11,6 +14,16 @@ public class CountryService {
     }
 
     public Country create(String name) {
+        validateName(name);
         return countryDao.insert(name);
+    }
+
+    private void validateName(String name) {
+        if(name == null || name.trim().isEmpty()) {
+            throw new InvalidArgumentDhexException(EMPTY_NAME_ERROR_MESSAGE);
+        }
+        if(name.length() > 100) {
+            throw new InvalidArgumentDhexException(LARGE_NAME_ERROR_MESSAGE);
+        }
     }
 }
--- src/test/java/com/dhex/shipping/services/CountryServiceTest.java
+++ src/test/java/com/dhex/shipping/services/CountryServiceTest.java
@@ -1,15 +1,17 @@
 package com.dhex.shipping.services;
 
 import com.dhex.shipping.dao.CountryDao;
+import com.dhex.shipping.exceptions.InvalidArgumentDhexException;
 import com.dhex.shipping.model.Country;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -19,10 +21,12 @@ public class CountryServiceTest {
     private CountryService countryService;
     @Mock
     private CountryDao countryDao;
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
 
     @Before
     public void setUp() {
-        countryService =new CountryService(countryDao);
+        countryService = new CountryService(countryDao);
     }
 
     @Test
@@ -39,8 +43,38 @@ public class CountryServiceTest {
 
         // assert
         assertThat(country.isEnabled(), is(true));
-        assertEquals(country.getId(), expectedId);
+        assertThat(country.getId(), is(expectedId));
         assertThat(country.getName(), is(countryName));
     }
 
+    @Test
+    public void shouldHaveNameAsNotNullWhenCreateCountry() {
+        // assert
+        expectedException.expect(InvalidArgumentDhexException.class);
+        expectedException.expectMessage("Name of the country should not be empty");
+
+        // act
+        countryService.create(null);
+    }
+
+    @Test
+    public void shouldHaveNameAsNotEmptyWhenCreateCountry() {
+        // assert
+        expectedException.expect(InvalidArgumentDhexException.class);
+        expectedException.expectMessage("Name of the country should not be empty");
+
+        // act
+        countryService.create(" ");
+    }
+
+    @Test
+    public void shouldThrowAnExceptionIfNameIsLarge() {
+        // assert
+        expectedException.expect(InvalidArgumentDhexException.class);
+        expectedException.expectMessage("Name of the country should not be greater than 100 chars");
+
+        // act
+        String largeCountryName = "A really really long country name in a far far far far far far far far far far universe from a really different dimension of the future past";
+        countryService.create(largeCountryName);
+    }
 }
