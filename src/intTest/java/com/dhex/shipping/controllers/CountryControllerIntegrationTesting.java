ef99fadca0c68d6ba685a3dbba1a9e0a3e64c391
Paolo Carrasco <paolo.carrasco@gmail.com>
2016-09-14 21:58:25 -0500

    we validate if duplicated country returns 404 on creation

--- src/intTest/java/com/dhex/shipping/controllers/CountryControllerIntegrationTesting.java
+++ src/intTest/java/com/dhex/shipping/controllers/CountryControllerIntegrationTesting.java
@@ -2,6 +2,8 @@ package com.dhex.shipping.controllers;
 
 import com.dhex.shipping.Application;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
@@ -10,29 +12,43 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        classes = Application.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CountryControllerIntegrationTesting {
+
     @LocalServerPort
     private int port;
 
+    @Before
     public void setup() {
         RestAssured.port = port;
     }
 
     @Test
     public void shouldReturn201WhenCountryIsCreated() {
-        given()
-                .body("Peru")
-        .when()
-                .post("/countries")
-        .then()
+        createCountry("Peru").then()
                 .assertThat()
                 .statusCode(CREATED.value())
                 .header("Location", "/country/Peru")
                 .body("name", is("Peru"));
+    }
+
+    @Test
+    public void shouldReturn400WhenCountryAlreadyExists() {
+        createCountry("Brasil");
+        createCountry("Brasil").then()
+                .assertThat()
+                .statusCode(BAD_REQUEST.value());
+    }
+
+    private Response createCountry(String countryName) {
+        return given()
+                .body(countryName)
+        .when()
+                .post("/countries");
+
     }
 }
